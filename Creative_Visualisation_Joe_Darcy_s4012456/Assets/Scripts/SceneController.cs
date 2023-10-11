5382e03cdb08490c2121ed0b97718c3fdd1dda34
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2023-10-11 12:35:51 +0100

    Quit function added.

--- Creative_Visualisation_Joe_Darcy_s4012456/Assets/Scripts/SceneController.cs
+++ Creative_Visualisation_Joe_Darcy_s4012456/Assets/Scripts/SceneController.cs
@@ -7,6 +7,27 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField] private int sceneToLoad;
 
+    private SpriteRenderer spriteRenderer;
+    private Color colour;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        colour = spriteRenderer.color;
+        colour.a = 0.2f;
+        spriteRenderer.color = colour;
+    }
+
+    private void OnMouseExit()
+    {
+        colour = spriteRenderer.color;
+        colour.a = 0.05f;
+        spriteRenderer.color = colour;
+    }
 
     private void OnMouseDown()
     {
